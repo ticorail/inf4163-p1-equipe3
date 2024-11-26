e53133531895df6d4cdbe516c9404ccbe25027d5
Bryan Gabriel-Atangana Mboa <137491003+BryanGabrielAtangana@users.noreply.github.com>
2024-11-26 18:53:57 -0500

    Update Creation_Des_Tables.sql

--- Creation_Des_Tables.sql
+++ Creation_Des_Tables.sql
@@ -1,58 +1,46 @@
-
-
 CREATE TABLE magasin (
-    id_magasin INT PRIMARY KEY not null,
+    id_magasin INT PRIMARY KEY NOT NULL,
     nom_magasin VARCHAR(50) NOT NULL,
     ville VARCHAR(255) NOT NULL,
     adresse VARCHAR(255) NOT NULL,
-    type_magasin Varchar(50) NOT NULL
+    type_magasin VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Promotion (
-	id_promotion INT PRIMARY KEY,
-	type_promtion VARCHAR(50) NOT NULL,
-	descriptions TEXT,
-	date_debut DATETIME NOT NULL,
-	date_fin DATETIME NOT NULL
-	
+    id_promotion INT PRIMARY KEY,
+    type_promotion VARCHAR(50) NOT NULL,
+    description_promotion TEXT,
+    date_debut DATETIME NOT NULL,
+    date_fin DATETIME NOT NULL
 );
 
 CREATE TABLE client (
-	id_client bigint not null,
-	nom_client varchar(255) not null,
-	categorie_client varchar(50) 
-
+    id_client BIGINT PRIMARY KEY NOT NULL,
+    nom_client VARCHAR(255) NOT NULL,
+    categorie_client VARCHAR(50)
 );
 
-
-
-CREATE TABLE [transaction](
-	id_transaction bigint primary key not null,
-	[date] datetime not null,
-	montant_total decimal( 10,2) not null,
-	saison varchar(50) not null,
-	produits TEXT not null,
-	methode_paiement varchar(255) not null,
-	promotion_appliquee TEXT,
-	FOREIGN KEY (id_magasin) REFERENCES magasin(id_magasin),
-	FOREIGN KEY (id_client) REFERENCES client(id_client)
+CREATE TABLE transaction (
+    id_transaction BIGINT PRIMARY KEY NOT NULL,
+    id_magasin INT NOT NULL,
+    id_client BIGINT NOT NULL,
+    date_transaction DATETIME NOT NULL,
+    montant_total DECIMAL(10, 2) NOT NULL,
+    saison VARCHAR(50) NOT NULL,
+    produits TEXT NOT NULL,
+    methode_paiement VARCHAR(255) NOT NULL,
+    promotion_appliquee TEXT,
+    FOREIGN KEY (id_magasin) REFERENCES magasin(id_magasin),
+    FOREIGN KEY (id_client) REFERENCES client(id_client)
 );
 
-CREATE TABLE produit(
-	id_produit int primary key not null,
-	nom_produit varchar(50) not null,
-	category_produit varchar(50),
-	quantite_en_stock int not null,
-	prix_unitaire decimal (10,2) not null,
-	[description] text
-	foreign key (Id_promotion) references promotion(id_promotion)
-
+CREATE TABLE produit (
+    id_produit INT PRIMARY KEY NOT NULL,
+    nom_produit VARCHAR(50) NOT NULL,
+    category_produit VARCHAR(50),
+    quantite_en_stock INT NOT NULL,
+    prix_unitaire DECIMAL(10, 2) NOT NULL,
+    description_produit TEXT,
+    id_promotion INT,
+    FOREIGN KEY (id_promotion) REFERENCES Promotion(id_promotion)
 );
-
-
-
-
-
-
-
-
