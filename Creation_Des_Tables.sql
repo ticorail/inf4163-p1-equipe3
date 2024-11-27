c0595093df37a17d5720c007384366adc0434741
Bryan Gabriel-Atangana Mboa <137491003+BryanGabrielAtangana@users.noreply.github.com>
2024-11-26 19:20:29 -0500

    Update Creation_Des_Tables.sql

--- Creation_Des_Tables.sql
+++ Creation_Des_Tables.sql
@@ -1,13 +1,13 @@
 CREATE TABLE magasin (
-    id_magasin INT PRIMARY KEY NOT NULL,
+    id_magasin INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nom_magasin VARCHAR(50) NOT NULL,
     ville VARCHAR(255) NOT NULL,
     adresse VARCHAR(255) NOT NULL,
     type_magasin VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Promotion (
-    id_promotion INT PRIMARY KEY,
+CREATE TABLE promotion (
+    id_promotion INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     type_promotion VARCHAR(50) NOT NULL,
     description_promotion TEXT,
     date_debut DATETIME NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE Promotion (
 );
 
 CREATE TABLE client (
-    id_client BIGINT PRIMARY KEY NOT NULL,
+    id_client BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nom_client VARCHAR(255) NOT NULL,
     categorie_client VARCHAR(50)
 );
@@ -30,12 +30,14 @@ CREATE TABLE transaction (
     produits TEXT NOT NULL,
     methode_paiement VARCHAR(255) NOT NULL,
     promotion_appliquee TEXT,
-    FOREIGN KEY (id_magasin) REFERENCES magasin(id_magasin),
+    FOREIGN KEY (id_magasin) REFERENCES magasin(id_magasin)
+        ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_client) REFERENCES client(id_client)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE produit (
-    id_produit INT PRIMARY KEY NOT NULL,
+    id_produit INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nom_produit VARCHAR(50) NOT NULL,
     category_produit VARCHAR(50),
     quantite_en_stock INT NOT NULL,
@@ -43,4 +45,5 @@ CREATE TABLE produit (
     description_produit TEXT,
     id_promotion INT,
     FOREIGN KEY (id_promotion) REFERENCES Promotion(id_promotion)
+        ON DELETE SET NULL ON UPDATE CASCADE
 );
